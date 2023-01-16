014242a97bf3089f0a691f6eaa51863c50d46c58
ry3soul <121807404+ry3soul@users.noreply.github.com>
2023-01-16 11:17:26 +0000

    Update login.php

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
     $email   = $_POST['email'];
     $password = $_POST['password'];
 
+// if else for the log in validation making sure its the correct login
 
     if (($VAILD_EMAIL == $email) && ($VAILD_PASSWORD == $password)) {
         correctlogin($Email);
@@ -23,7 +24,7 @@
         header("location:home.php");
         exit();
 
-    }
+    }                                               //using functions to redirect to pages 
 
     function incorrectlogin ($email) {
         error_log("Login accepted for " . $email,0);
